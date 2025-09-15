b1585e1795cdd0f2c6567eaf793eea4f30e26b2d
auroralevanti <auroralevanti@gmail.com>
2025-09-15 17:52:47 -0400

    login to back listo y funcional

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,22 +9,62 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@radix-ui/react-separator";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 
 
 interface LoginForm {
-  user: string;
+  email: string;
   password: string;
 };
 
 export default function LoginPage() {
 
-  const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>();
+  const router = useRouter();
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<LoginForm>();
 
 
-  const submitLogin: SubmitHandler<LoginForm> = ({ user, password }: LoginForm) => {
-    console.log(user, password);
-    redirect('/blog')
+  const submitLogin: SubmitHandler<LoginForm> = async ({ email, password }: LoginForm) => {
+    console.log(email, password);
+
+    const url = 'https://codequest-backend-2025.onrender.com/api/v1/auth/login';
+
+    try {
+      
+      const toLogin = await fetch( url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+      });
+
+      const resp = await toLogin.json();
+      console.log(resp);
+
+   /*    if( resp.status === 401 ){
+        alert('Error en email o password');
+      }; */
+      if( resp ){
+        alert('Bienvenido');
+        router.push('/blog')
+      }
+
+      if( resp.message === "Invalid password" ){
+        reset()
+        alert('Password incorrecto')
+      };
+
+      if( resp.message[0] === "password must be longer than or equal to 6 characters" ){
+        reset()
+        alert('Password debe contener al menos 6 caracteres')
+      }
+
+
+    } catch (error) {
+      
+    }
+
   };
 
   return (
@@ -46,7 +86,7 @@ export default function LoginPage() {
             <Input
               className="text-white mt-2"
               placeholder="Correo Electronico"
-              {...register('user', { required: true })}
+              {...register('email', { required: true })}
             >
             </Input>
             <Input
@@ -67,11 +107,15 @@ export default function LoginPage() {
           </form>
           <div className="flex items-center mb-5">
             <Separator className="flex-grow border-gray-300 border-1 mr-1" />
-              <span className="text-white">o continua con </span>
+              <span className="text-white">&nbsp; o continua con &nbsp;</span>
             <Separator className="flex-grow border-gray-300 border-1 ml-1" />
           </div>
           <div className="text-white mb-10 justify-center items-center">
-            Discord / GitHub
+            <Button 
+            variant='outline'
+            className="justify-center items-center">
+              Discord
+            </Button>
           </div>
           <div className="flex flex-row mb-10">
             <p className="text-white">¿Quieres ser miembro? &nbsp;</p>
