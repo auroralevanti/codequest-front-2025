d770a8221adddafa5b8f1eaa1ecf8dcee982ad41
RodolfoXiloj <77630343+RodolfoXiloj@users.noreply.github.com>
2025-09-18 22:36:16 -0600

    feat: remove mockPost

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,23 +7,6 @@ import { apiUrls } from '@/config/api';
 import { getUserCookie } from '@/lib/cookies';
 
 const mockUser = { id: 'u1', username: 'Jordyn George', avatarUrl: 'https://i.ytimg.com/vi/PhOEqsmWpdg/maxresdefault.jpg' };
-const mockPost = {
-  id: 'p1',
-  user: mockUser,
-  createdAt: 'Wednesday',
-  body: 'Hola DeviHola DeviHola DeviHola DeviHola DeviHola Devi',
-  images: [
-    'https://i.ytimg.com/vi/PhOEqsmWpdg/maxresdefault.jpg',
-    'https://media.licdn.com/dms/image/D4E22AQERqd-FCUZA0g/feedshare-shrink_2048_1536/0/1707582515866?e=2147483647&v=beta&t=ovFCGyNnPYduDuADvD2J5q5V5E44ZTHqZe_9OqeGpv4',
-    'https://media.licdn.com/dms/image/v2/D5622AQHYxabjRY81fA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1711552820275?e=2147483647&v=beta&t=yqN7G8-2Bcu1O-LY1ipW5NQ3ipa1eKAwjmmwDDEEjJE',
-    'https://i.ytimg.com/vi/PhOEqsmWpdg/maxresdefault.jpg',
-    'https://media.licdn.com/dms/image/D4E22AQERqd-FCUZA0g/feedshare-shrink_2048_1536/0/1707582515866?e=2147483647&v=beta&t=ovFCGyNnPYduDuADvD2J5q5V5E44ZTHqZe_9OqeGpv4',
-    'https://media.licdn.com/dms/image/v2/D5622AQHYxabjRY81fA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1711552820275?e=2147483647&v=beta&t=yqN7G8-2Bcu1O-LY1ipW5NQ3ipa1eKAwjmmwDDEEjJE',
-    'https://i.ytimg.com/vi/PhOEqsmWpdg/maxresdefault.jpg',
-  ],
-  likes: 220000,
-  comments: 19800,
-};
 
 export default function BlogPage() {
   const [posts, setPosts] = useState<any[]>([]);
